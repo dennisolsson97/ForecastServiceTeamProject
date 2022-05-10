02e2557aa639f718c38ee69e1c2fb2f45fd62f74
Dennis Olsson <dennisolofolsson@live.se>
2022-05-10 10:56:39 +0200

    Initial commit

---
+++ src/main/java/se/iths/group/forecastserviceteamproject/ForecastServiceTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package se.iths.group.forecastserviceteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastServiceTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForecastServiceTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/group/forecastserviceteamproject/ForecastServiceTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.group.forecastserviceteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastServiceTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
