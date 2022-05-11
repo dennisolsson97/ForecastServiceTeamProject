f54dfacb18d46faa2d03cc8032410ec582cb5f6c
bjopa <bevan.pagler@iths.se>
2022-05-11 15:04:29 +0200

    Added third client and additional java classes

---
+++ src/main/java/se/iths/group/forecastserviceteamproject/weatherAPI/Hour.java
@@ -0,0 +1,46 @@
+
+package se.iths.group.forecastserviceteamproject.weatherAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "condition"
+})
+@Generated("jsonschema2pojo")
+public class Hour {
+
+    @JsonProperty("condition")
+    private Condition__2 condition;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("condition")
+    public Condition__2 getCondition() {
+        return condition;
+    }
+
+    @JsonProperty("condition")
+    public void setCondition(Condition__2 condition) {
+        this.condition = condition;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/se/iths/group/forecastserviceteamproject/weatherAPI/Location.java
@@ -0,0 +1,137 @@
+
+package se.iths.group.forecastserviceteamproject.weatherAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "region",
+    "country",
+    "lat",
+    "lon",
+    "tz_id",
+    "localtime_epoch",
+    "localtime"
+})
+@Generated("jsonschema2pojo")
+public class Location {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("region")
+    private String region;
+    @JsonProperty("country")
+    private String country;
+    @JsonProperty("lat")
+    private Double lat;
+    @JsonProperty("lon")
+    private Double lon;
+    @JsonProperty("tz_id")
+    private String tzId;
+    @JsonProperty("localtime_epoch")
+    private Integer localtimeEpoch;
+    @JsonProperty("localtime")
+    private String localtime;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("region")
+    public String getRegion() {
+        return region;
+    }
+
+    @JsonProperty("region")
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @JsonProperty("country")
+    public String getCountry() {
+        return country;
+    }
+
+    @JsonProperty("country")
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @JsonProperty("lat")
+    public Double getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    @JsonProperty("lon")
+    public Double getLon() {
+        return lon;
+    }
+
+    @JsonProperty("lon")
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    @JsonProperty("tz_id")
+    public String getTzId() {
+        return tzId;
+    }
+
+    @JsonProperty("tz_id")
+    public void setTzId(String tzId) {
+        this.tzId = tzId;
+    }
+
+    @JsonProperty("localtime_epoch")
+    public Integer getLocaltimeEpoch() {
+        return localtimeEpoch;
+    }
+
+    @JsonProperty("localtime_epoch")
+    public void setLocaltimeEpoch(Integer localtimeEpoch) {
+        this.localtimeEpoch = localtimeEpoch;
+    }
+
+    @JsonProperty("localtime")
+    public String getLocaltime() {
+        return localtime;
+    }
+
+    @JsonProperty("localtime")
+    public void setLocaltime(String localtime) {
+        this.localtime = localtime;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
