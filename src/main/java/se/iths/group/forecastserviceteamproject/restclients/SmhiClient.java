8e7157bbb6ba7a4d00b43cc4d6cd116151147f0f
s_debachtin <simon.bachtin@iths.se>
2022-05-12 13:18:44 +0200

    Change stream to foreach in order to get Double value without putting into lists

--- src/main/java/se/iths/group/forecastserviceteamproject/restclients/SmhiClient.java
+++ src/main/java/se/iths/group/forecastserviceteamproject/restclients/SmhiClient.java
@@ -13,54 +13,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SmhiClient {
+
     RestTemplate restTemplate = new RestTemplate();
     String baserUrl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0300/lat/59.3110/data.json";
     ResponseEntity<ForecastSmhi> responseAllData = restTemplate.exchange(baserUrl, HttpMethod.GET, null, ForecastSmhi.class);
 
-    public static void main(String[] args) {
-        SmhiClient smhiClient = new SmhiClient();
-        smhiClient.getTime();
-        smhiClient.getTemperature();
-        smhiClient.getHumidity();
-        //smhiClient.getParameterData();
+    ForecastSmhi forecastSmhi = responseAllData.getBody();
 
-    }
+    public String getTime() {
 
-    public String getTime(){
-        ForecastSmhi forecastSmhi = responseAllData.getBody();
-        String time = forecastSmhi.getTimeSeries().get(23).getValidTime();
-        return time;
+        return forecastSmhi.getTimeSeries().get(23).getValidTime();
     }
 
     public Double getTemperature() {
-        ForecastSmhi forecastSmhi = responseAllData.getBody();
+
         List<Parameter> parameters = forecastSmhi.getTimeSeries().get(23).getParameters();
-        List<List<Double>> temperature = parameters.stream()
-                .filter(p -> p.getName().equals("t"))
-                .map(p -> p.getValues()).collect(Collectors.toList());
+        Double temperature = 0.0;
 
-        Double doubleTemperature = temperature.get(0).get(0);
+        for (Parameter p: parameters) {
+            if (p.getName().equals("t"))
+             temperature = p.getValues().get(0);
+        }
 
-
-
-
-            return doubleTemperature;
+        return temperature;
 
     }
 
     public Double getHumidity() {
-        ForecastSmhi forecastSmhi = responseAllData.getBody();
+
         List<Parameter> parameters = forecastSmhi.getTimeSeries().get(23).getParameters();
+        Double humidity = 0.0;
 
-       List<List<Double>> doubleList = parameters.stream()
-                .filter(p -> p.getName().equals("r"))
-                .map(p -> p.getValues()).collect(Collectors.toList());
-
-        Double doubleHumidity = doubleList.get(0).get(0);
-
-        return doubleHumidity;
-
-
+        for (Parameter p: parameters) {
+            if (p.getName().equals("r"))
+                humidity = p.getValues().get(0);
+        }
+        return humidity;
     }
-
 }
